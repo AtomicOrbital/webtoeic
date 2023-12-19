11dd5a787aafbf5af9c26fed70589fe4ccb8ea2f
NguyenThanhDat <topthuthuat69@gmail.com>
2023-12-19 20:47:28 +0700

    completed firebase

--- src/main/java/com/example/webtoeic/config/FirebaseConfig.java
+++ src/main/java/com/example/webtoeic/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.example.webtoeic.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,16 +12,18 @@ import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase-create.token}")
+    private String firebaseToken;
+
     @Bean
     public void initialize() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("/media/thanhdat/data1/JavaCyber/JavaSpring/webtoeic/src/main/resources/box-chat-ef660-firebase-adminsdk-ooi2e-1c252a9bbc.json");
+        FileInputStream serviceAccount = new FileInputStream(firebaseToken);
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
 
         FirebaseApp.initializeApp(options);
-
     }
 }
